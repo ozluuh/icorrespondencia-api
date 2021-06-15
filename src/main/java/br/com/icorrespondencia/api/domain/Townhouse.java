097306fd575ccfdff1298dc47b5b884f9b9e9011
ozluuh <ozluuh@gmail.com>
2021-06-15 03:34:39 -0300

    feat: add initial jpa configuration
    Add H2 configuration to prepare tests

--- src/main/java/br/com/icorrespondencia/api/domain/Townhouse.java
+++ src/main/java/br/com/icorrespondencia/api/domain/Townhouse.java
@@ -1,5 +1,10 @@
 package br.com.icorrespondencia.api.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Townhouse {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
