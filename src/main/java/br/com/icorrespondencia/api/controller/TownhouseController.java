c64ffb3526b4936b06fec8a35991ff2f99782172
ozluuh <ozluuh@gmail.com>
2021-06-15 00:31:58 -0300

    feat: add controller with index endpoint

---
+++ src/main/java/br/com/icorrespondencia/api/controller/TownhouseController.java
@@ -0,0 +1,25 @@
+package br.com.icorrespondencia.api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.icorrespondencia.api.domain.Townhouse;
+import br.com.icorrespondencia.api.service.TownhouseService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/townhouses")
+public class TownhouseController {
+
+    private final TownhouseService service;
+
+    @GetMapping
+    public List<Townhouse> index() {
+        return service.index().orElse(new ArrayList<>());
+    }
+}
