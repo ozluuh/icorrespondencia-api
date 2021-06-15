51a59ec93a3f170ce9fb0355b2112ee32461e213
ozluuh <ozluuh@gmail.com>
2021-06-15 00:31:19 -0300

    feat: add basic entity

---
+++ src/main/java/br/com/icorrespondencia/api/domain/Townhouse.java
@@ -0,0 +1,16 @@
+package br.com.icorrespondencia.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Townhouse {
+
+    private Long id;
+
+    private String name;
+
+}
