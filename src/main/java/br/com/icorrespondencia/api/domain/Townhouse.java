7e1d95ff7632bb4b5d64d619d038253a997fcdb0
ozluuh <ozluuh@gmail.com>
2021-06-16 18:25:55 -0300

    refactor(domain): add all fields to Townhouse

--- src/main/java/br/com/icorrespondencia/api/domain/Townhouse.java
+++ src/main/java/br/com/icorrespondencia/api/domain/Townhouse.java
@@ -1,10 +1,14 @@
 package br.com.icorrespondencia.api.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,13 +18,33 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "T_ICR_CONDOMINIO")
 @SequenceGenerator(name = "townhouse", allocationSize = 1, sequenceName = "SQ_ICR_CONDOMINIO")
 public class Townhouse {
 
     @Id
     @GeneratedValue(generator = "townhouse", strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_condominio", updatable = false)
     private Long id;
 
+    @Column(name = "ds_razao_social", nullable = false)
     private String name;
 
+    @Column(name = "nr_cnpj", columnDefinition = "CHAR(18) NOT NULL")
+    private String nin;
+
+    @Column(name = "ds_site", length = 100)
+    private String site;
+
+    @Column(name = "ds_email", length = 80)
+    private String email;
+
+    @Column(name = "dt_cadastro", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "dt_exclusao")
+    private LocalDateTime excludedAt;
+
+    @Column(name = "st_ativo", nullable = false)
+    private boolean active = true;
 }
