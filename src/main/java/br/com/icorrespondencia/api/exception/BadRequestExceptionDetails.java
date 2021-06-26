c605b5d136808d2118d05dc3bbebb6108965a45b
ozluuh <ozluuh@gmail.com>
2021-06-25 21:51:40 -0300

    refactor: change handler

--- src/main/java/br/com/icorrespondencia/api/exception/BadRequestExceptionDetails.java
+++
@@ -1,9 +0,0 @@
-package br.com.icorrespondencia.api.exception;
-
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-
-@Getter
-@SuperBuilder
-public class BadRequestExceptionDetails extends ExceptionDetails {
-}
--- src/main/java/br/com/icorrespondencia/api/handler/RestExceptionHandler.java
+++ src/main/java/br/com/icorrespondencia/api/handler/RestExceptionHandler.java
@@ -4,54 +4,74 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.icorrespondencia.api.exception.BadRequestException;
-import br.com.icorrespondencia.api.exception.BadRequestExceptionDetails;
+import br.com.icorrespondencia.api.exception.ExceptionDetails;
 import br.com.icorrespondencia.api.exception.ValidationExceptionDetails;
 import br.com.icorrespondencia.api.util.DateUtil;
 import br.com.icorrespondencia.api.util.DetailsExceptionUtil;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException exception) {
+    protected ResponseEntity<Object> handleBadRequest(
+        BadRequestException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-        return new ResponseEntity<>(
-                BadRequestExceptionDetails
-                    .builder()
-                    .timestamp(DateUtil.formatDateTimeToSQL(LocalDateTime.now()))
-                    .status(HttpStatus.BAD_REQUEST.value())
-                    .title(DetailsExceptionUtil.formatTitle(exception.getClass().getSimpleName()))
-                    .details(exception.getMessage())
-                    .build(), HttpStatus.BAD_REQUEST);
+        return handleExceptionInternal(ex, ex.getMessage(), headers, HttpStatus.BAD_REQUEST, request);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-        List<String> messages = exception
+        List<String> messages = ex
                 .getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.toList());
 
-        return new ResponseEntity<>(
-                ValidationExceptionDetails
-                    .builder()
-                    .timestamp(DateUtil.formatDateTimeToSQL(LocalDateTime.now()))
-                    .status(HttpStatus.BAD_REQUEST.value())
-                    .title(DetailsExceptionUtil.formatTitle(exception.getClass().getSimpleName()))
-                    .details("Validation failed, check messages bellow")
-                    .messages(messages)
-                    .build(), HttpStatus.BAD_REQUEST);
+        return handleExceptionInternal(ex, messages, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails details = ExceptionDetails
+            .builder()
+            .status(status.value())
+            .title(DetailsExceptionUtil.formatTitle(ex.getClass().getSimpleName()))
+            .details((String) body)
+            .timestamp(DateUtil.formatDateTimeToSQL(LocalDateTime.now()))
+            .build();
+
+            return new ResponseEntity<>(details, headers, status);
+    }
+
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, List<String> messages, HttpHeaders headers, HttpStatus status) {
+
+        ValidationExceptionDetails details = ValidationExceptionDetails
+            .builder()
+            .status(status.value())
+            .title(DetailsExceptionUtil.formatTitle(ex.getClass().getSimpleName()))
+            .details("Validation failed, check messages bellow")
+            .timestamp(DateUtil.formatDateTimeToSQL(LocalDateTime.now()))
+            .messages(messages)
+            .build();
+
+            return new ResponseEntity<>(details, headers, status);
     }
 }
