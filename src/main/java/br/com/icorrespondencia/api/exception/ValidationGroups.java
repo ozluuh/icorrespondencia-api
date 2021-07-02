1249a66f1c22440dd2127777ea4ae8acd064cec7
ozluuh <ozluuh@gmail.com>
2021-07-02 00:01:28 -0300

    feat: add ValidationGroups
    with this payload has validation through http verb

---
+++ src/main/java/br/com/icorrespondencia/api/exception/ValidationGroups.java
@@ -0,0 +1,11 @@
+package br.com.icorrespondencia.api.exception;
+
+import javax.validation.groups.Default;
+
+public interface ValidationGroups {
+    interface Put extends Default {
+    }
+
+    interface Post extends Default {
+    }
+}
