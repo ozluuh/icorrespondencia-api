868ac59a3476d5c7bb19503c84efd8715604200a
ozluuh <ozluuh@gmail.com>
2021-07-02 01:36:59 -0300

    build: add annotation to exclude from reports

--- src/main/java/br/com/icorrespondencia/api/ApiApplication.java
+++ src/main/java/br/com/icorrespondencia/api/ApiApplication.java
@@ -3,9 +3,12 @@ package br.com.icorrespondencia.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
 @SpringBootApplication
 public class ApiApplication {
 
+    @Generated
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
