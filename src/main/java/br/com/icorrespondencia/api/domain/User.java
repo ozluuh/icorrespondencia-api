08a8a5530b323d4a8ea1a067d702084722ae8393
ozluuh <ozluuh@gmail.com>
2021-09-19 22:30:30 -0300

    refactor: add validation to user model

--- src/main/java/br/com/icorrespondencia/api/domain/User.java
+++ src/main/java/br/com/icorrespondencia/api/domain/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -24,13 +26,18 @@ import lombok.ToString;
 @DiscriminatorValue("F")
 public class User extends Person {
 
+    private static final String ENTER_A_VALID_MAIL_VALIDATION_MESSAGE = "Enter a valid email";
+
+    @NotEmpty(message = FIELD_MUST_BE_FILLED_VALIDATION_MESSAGE)
     @Column(nullable = false, length = 20, unique = true)
     private String username;
 
     @JsonView(View.Internal.class)
+    @NotEmpty(message = FIELD_MUST_BE_FILLED_VALIDATION_MESSAGE)
     @Column(nullable = false, length = 64)
     private String password;
 
     @JsonView(View.Internal.class)
+    @Email(message = ENTER_A_VALID_MAIL_VALIDATION_MESSAGE, regexp = "^[A-z0-9\\.]+@[A-z0-9]+\\.[A-z]{1,}\\.?[A-z]+$")
     private String email;
 }
