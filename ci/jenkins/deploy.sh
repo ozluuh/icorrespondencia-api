5840be354243c1fc2679f3af9b712f9a06fd25ea
ozluuh <ozluuh@gmail.com>
2021-10-22 02:36:17 -0300

    build: update deploy script

--- ci/jenkins/deploy.sh
+++ ci/jenkins/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 get_deploy_url() {
-    cat log.txt | grep -oE "https?.*\.net"
+    cat temp/log.txt | grep -oE "https?.*\.net"
     rm -f log.txt
 }
 
 make_deploy() {
-    mvn azure-webapp:deploy > log.txt
+    # mvn azure-webapp:deploy > temp/log.txt
+    if [[ ! -e "temp/log.txt" ]]; then
+        echo 1
+        return 1
+    fi
+
+    echo 0
 }
 
 "$@"
